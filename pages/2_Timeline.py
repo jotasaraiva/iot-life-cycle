711cce0c1e130da825e86b7ad99bf3a4fb77b322
jotasaraiva <jpsaraiva2002@gmail.com>
2025-01-07 15:01:25 -0300

    Test timeline

--- pages/2_Timeline.py
+++ pages/2_Timeline.py
@@ -36,19 +36,9 @@ if st.session_state['authentication_status']:
     rows = execute_query(conn.table("timeline").select("*"), ttl="5m")
     time_data = pd.DataFrame(rows.data)
 
-    bars_data = (
-        time_data
-            .groupby(['status', 'data'])
-            .size()
-            .reset_index(name='counts')
-            .sort_values(ascending=True, by=['status', 'data'])
-    )
+    time_data = time_data.sort_values(ascending=True, by='data')
 
-    bars_data_long = utils.expand_dates_by_group(bars_data, 'data', 'status')
-    bars_data_wide = bars_data_long.pivot(index="data", columns="status", values="counts").reset_index().fillna(0)
-
-    st.dataframe(bars_data_long, use_container_width=True, hide_index=True)
-    st.dataframe(bars_data_wide, use_container_width=True, hide_index=True)
+    st.dataframe(time_data)
 
     # Logout
     st.session_state['authenticator'].logout(location='sidebar')
