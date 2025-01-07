76641e73d61065e98a5c380bcdac7405f241f001
jotasaraiva <jpsaraiva2002@gmail.com>
2025-01-07 00:34:14 -0300

    Timeline testing

--- pages/2_Timeline.py
+++ pages/2_Timeline.py
@@ -20,7 +20,7 @@ with st.sidebar:
 def load_css(file_path):
     with open(file_path) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-css_path = pathlib.Path(__file__).parents[1] / "assets" / "stock_styles.css"
+css_path = pathlib.Path(__file__).parents[1] / "assets" / "tl_styles.css"
 load_css(css_path)
 
 # Check authentication state
@@ -36,11 +36,22 @@ if st.session_state['authentication_status']:
     rows = execute_query(conn.table("timeline").select("*"), ttl="5m")
     time_data = pd.DataFrame(rows.data)
 
-    st.dataframe(time_data, use_container_width=True, hide_index=True, height=625)
+    bars_data = (
+        time_data
+            .groupby(['status', 'data'])
+            .size()
+            .reset_index(name='counts')
+            .sort_values(ascending=True, by=['status', 'data'])
+    )
+
+    bars_data_long = utils.expand_dates_by_group(bars_data, 'data', 'status')
+    bars_data_wide = bars_data_long.pivot(index="data", columns="status", values="counts").reset_index().fillna(0)
+
+    st.dataframe(bars_data_long, use_container_width=True, hide_index=True)
+    st.dataframe(bars_data_wide, use_container_width=True, hide_index=True)
 
     # Logout
     st.session_state['authenticator'].logout(location='sidebar')
 
 if st.session_state['authentication_status'] == False or st.session_state['authentication_status'] == None:
     st.switch_page('Home.py')
-
--- src/utils.py
+++ src/utils.py
@@ -177,6 +177,20 @@ def get_last_values_by_date(df, match_column, match_value, target_column, date_c
     else:
         return None
 
+def expand_dates_by_group(df, date_column, group_column):
+        
+        df[date_column] = pd.to_datetime(df[date_column])
+        expanded_df = pd.DataFrame()
+        for group, group_data in df.groupby(group_column):
+            full_date_range = pd.date_range(start=group_data[date_column].min(), 
+                                            end=group_data[date_column].max())
+            group_data = group_data.set_index(date_column).reindex(full_date_range)
+            group_data = group_data.reset_index().rename(columns={"index": date_column})
+            group_data[group_column] = group
+            expanded_df = pd.concat([expanded_df, group_data], ignore_index=True)
+
+        return expanded_df
+
 # Global vars
 clientes = [
     "ELKS Engenharia Florestal e Ambiental Ltda",
