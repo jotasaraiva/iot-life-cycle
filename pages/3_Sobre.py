0132e901abe913e763257c1562ed9fbb8a46f82d
jotasaraiva <jpsaraiva2002@gmail.com>
2025-01-16 10:29:45 -0300

    Remove drawio

---
+++ pages/3_Sobre.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from src import utils
+import pathlib
+
+# Page config
+st.set_page_config(page_title="Treevia LC - Timeline", layout='wide')
+
+# Logo
+logo_path="assets/treevia-logo.png"
+name_path="assets/treevia-name.png"
+st.logo(logo_path)
+with st.sidebar:
+    st.image(name_path)
+
+# CSS config
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+css_path = pathlib.Path(__file__).parents[1] / "assets" / "tl_styles.css"
+load_css(css_path)
+
+# Check authentication state
+if 'authenticator' not in st.session_state:
+    st.switch_page('Home.py')
+if st.session_state['authentication_status'] == False or st.session_state['authentication_status'] == None:
+    st.switch_page('Home.py')
+
+# Layout
+if st.session_state['authentication_status']:
+
+    # Logout
+    utils.log_out()
+
+if st.session_state['authentication_status'] == False or st.session_state['authentication_status'] == None:
+    st.switch_page('Home.py')
